2f512f57497a87de5f156da81ece3aa16dbc6ac9
Gladys777 <gladysnader@hotmail.com>
2022-08-13 09:41:49 -0500

    actualizacion

--- src/main/java/com/Ciclo3/Inicio/Inicio.java
+++ src/main/java/com/Ciclo3/Inicio/Inicio.java
@@ -3,6 +3,7 @@ package com.Ciclo3.Inicio;
 public class Inicio {
 
     public static void main(String[] args)   {
-
+        System.out.println("Hola Developers");
+        System.out.println("Hola Developers");
     }
 }
